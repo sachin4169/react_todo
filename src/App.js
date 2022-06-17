97c1e0ba8790cda4e84fc970d4065dbcc1ddd180
sachinyadav <sachinyadav@cedcoss.com>
2022-06-17 10:47:08 +0530

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import "./app.css";
+import List from './List';
+import Aos from 'aos';
+import "aos/dist/aos.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [show, setShow] = useState(true)
+    const [text, setText] = useState('')
+    const [list, setList] = useState([])
+    const [id, setid] = useState(0)
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, []);
+
+    const handleChange = (event) => {
+        setText(event.target.value);
+    }
+    
+    const handleclick = () => {
+        setList(prevlist => {
+            return [...prevlist, text]
+        });
+        setText("");
+    }
+
+    function handledelete(id){
+        console.log('clicked delete'+ id);
+        setList(prevlist => {
+            return prevlist.filter((list,index) => {
+                return index !== id
+            })
+        });
+    }
+
+    const handleEdit = (id) =>{
+        console.log('clicked edit'+ id);
+       
+        const item = list.find((element,index)=>{
+            return index===id
+        })
+        console.log(item);
+        setText(item);
+        setid(id)
+        setShow(false)
+    }
+
+    const handleUpdate = ()=>{
+        setList(prevlist => {
+            prevlist[id]=text;
+        return [...prevlist];
+        });
+        setShow(true);
+        setText("");
+    }
+
+    return (<>
+        <div className='container' data-aos="zoom-in">
+            <div className='bg'>
+            </div>
+        </div>
+        <div className='todoBox' data-aos="fade-down">
+            <h1>TODO</h1>
+            <input
+                data-aos="fade-right"
+                className='inputbox'
+                type="text"
+                value={text}
+                onChange={handleChange}
+            />
+            {show ? <i id='add' className="fa-solid fa-plus add" onClick={handleclick}></i>:
+            <i className="fa-solid fa-pen-to-square add" onClick={handleUpdate}></i>
+            }
+            
+            <div data-aos="fade-left" className='content'>
+                {list.map((element, index) => {
+                    return (
+                        <List
+                        key = {index}
+                        id={index}
+                        item = {element}
+                        onDelete = {handledelete}
+                        onEdit = {handleEdit}
+                        />)
+                })}
+
+            </div>
+        </div></>
+    )
 }
 
-export default App;
+export default App
---
+++ src/List.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const List = (props) => {
+   
+
+    function handleClickDelete(){
+        props.onDelete(props.id)
+    }
+    function handleClickEdit(){
+        props.onEdit(props.id)
+    }
+    return (
+        <div className='list' data-aos="flip-up">
+            <input type="checkbox" />
+            <p>{props.item}</p>
+            <div>
+                <i style={{ color: "#50c870", padding: "3px", cursor: "pointer" }} 
+                className="fa-solid fa-pen-to-square fa-lg"
+                onClick={handleClickEdit}
+                ></i>
+                <i style={{ color: "#df3c3c", padding: "3px", cursor: "pointer" }} 
+                className="fa-solid fa-trash-can fa-lg"
+                onClick={handleClickDelete}
+                ></i>
+            </div>
+        </div>
+    )
+}
+
+export default List
